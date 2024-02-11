693aeb9aca3e9ab131189fd8e946d228b072a2d1
hammadali802 <125157992+hammadali802@users.noreply.github.com>
2024-02-12 00:31:20 +0100

    Add files via upload

---
+++ sweprojekt.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Content-Type text/html"
+echo
+echo'
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <link href="stylesheet_sweprojekt.css" rel="stylesheet">
+    <title>Kalorienbedarfsrechner</title>
+  </head>
+  <body>
+    <div class="logo">
+      <a href="https://informatik.hs-bremerhaven.de/step2022team06/projekt/sweprojekt.html">
+        <img src="lasagne_impact_logo.png" alt="team06_logo" style="max-width: 224px; max-height: 384px;">
+      </a>
+    </div>
+    <div class="header">
+      <h1 style="margin-right: 168px;">Kalorienrechner Team06</h1>  
+    </div>
+    <div class="navigation">
+      <ul class="navlist_ul">
+        <li class="navlist_li">
+          <a class="nav_a" href="https://www.wehorse.com/de/blog/wp-content/uploads/2022/01/Reiten-lernen-fu%CC%88r-Erwachsene-1536x864.jpg">3. REITER</a>
+        </li>
+        <li class="navlist_li">
+          <a class="nav_a" href="https://informatik.hs-bremerhaven.de/step2022team06/">LASAGNE IMPACT</a>
+        </li>
+        <li class="navlist_li">
+          <a id="cur_site" class="nav_a" href="https://informatik.hs-bremerhaven.de/step2022team06/projekt/sweprojekt.html">KALORIENRECHNER</a>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      d
+    </div>
+    <div class="footer">
+      e
+    </div>
+  </body>
+</html>'
